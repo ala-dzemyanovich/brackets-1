cce98250f60a35248b43373d93933d3f49f8ba94
Ala Dzemyanovich <ala.dzemyanovich@gmail.com>
2019-10-05 22:54:45 +0200

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    let bracketsArray = [].concat(...bracketsConfig);
+
+    let inverse = (symbol) => {
+        let ind = bracketsArray.indexOf(symbol);
+        if(ind === -1){
+            return symbol;
+        }
+        if(ind % 2 === 0) {
+            return bracketsArray[ind + 1];
+        }
+    }
+
+    let arr = [str[0]];
+    for(let i = 1; i < str.length; i++){
+        if(str[i] === inverse(arr[arr.length - 1])){
+            arr.pop();
+        }
+        else {
+            arr.push(str[i]);
+        }
+    }
+    return arr.length === 0;
 }
